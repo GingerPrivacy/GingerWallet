eba81bcf8ca39225b0cbcfd48f25e8ecebeebfe8
Roland Soós <soos.roland93@gmail.com>
2024-05-26 14:56:36 +0200

    Fix texts

--- WalletWasabi.Fluent/App.axaml.cs
+++ WalletWasabi.Fluent/App.axaml.cs
@@ -27,7 +27,7 @@ public class App : Application
 
 	public App()
 	{
-		Name = "Wasabi Wallet";
+		Name = "Ginger Wallet";
 	}
 
 	public App(Func<Task> backendInitialiseAsync, bool startInBg) : this()
