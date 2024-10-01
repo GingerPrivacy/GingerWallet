1c88587ff1c670e67af586a9a219837cf6004c86
Nagy Szabolcs <birkusz20@gmail.com>
2024-10-01 09:58:03 +0200

    mod hwi version

--- WalletWasabi/Helpers/Constants.cs
+++ WalletWasabi/Helpers/Constants.cs
@@ -91,7 +91,7 @@ public static class Constants
 
 	public static readonly Version ClientVersion = new(2, 0, 11, 0);
 
-	public static readonly Version HwiVersion = new("3.0.0");
+	public static readonly Version HwiVersion = new("3.1.0");
 	public static readonly Version BitcoinCoreVersion = new("23.0");
 	public static readonly Version GingerLegalDocumentsVersion = new(5, 0);
 
