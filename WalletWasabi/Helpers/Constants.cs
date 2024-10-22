1efd400cf89029c93c869591c56bf6e79d1c0cb4
Luzius 1089 <luzius1089@protonmail.com>
2024-10-22 11:40:27 +0200

    dump to 2.0.13 (#97)

--- WalletWasabi/Helpers/Constants.cs
+++ WalletWasabi/Helpers/Constants.cs
@@ -91,7 +91,7 @@ public static class Constants
 
 	public static readonly Money MaximumNumberOfBitcoinsMoney = Money.Coins(MaximumNumberOfBitcoins);
 
-	public static readonly Version ClientVersion = new(2, 0, 12, 0);
+	public static readonly Version ClientVersion = new(2, 0, 13, 0);
 
 	public static readonly Version HwiVersion = new("3.1.0");
 	public static readonly Version BitcoinCoreVersion = new("23.0");
