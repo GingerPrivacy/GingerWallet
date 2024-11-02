074c3b10464b4fffc9ee9583c2a29cb1c5e00c39
Luzius 1089 <luzius1089@protonmail.com>
2024-11-02 14:53:26 +0100

    add resync (#108)
    * add resync
    
    * add resync

---
+++ WalletWasabi.Fluent/ViewModels/Wallets/Settings/ResyncWalletViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ReactiveUI;
+using WalletWasabi.Fluent.ViewModels.Dialogs.Base;
+using WalletWasabi.Lang;
+
+namespace WalletWasabi.Fluent.ViewModels.Wallets.Settings;
+
+[NavigationMetaData(NavigationTarget = NavigationTarget.CompactDialogScreen)]
+public partial class ResyncWalletViewModel : DialogViewModelBase<bool>
+{
+	public ResyncWalletViewModel()
+	{
+		Title = Resources.ResyncWallet;
+		SetupCancel(false, true, true);
+		NextCommand = ReactiveCommand.Create(() => Close(DialogResultKind.Normal, true));
+	}
+}
