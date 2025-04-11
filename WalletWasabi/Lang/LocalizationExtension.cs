6964bd52b58261f4643ae40b1828ac8a5996d871
RolandUI <soos.roland93@gmail.com>
2025-04-11 13:31:38 +0200

    restore unwanted changes

--- WalletWasabi/Lang/LocalizationExtension.cs
+++ WalletWasabi/Lang/LocalizationExtension.cs
@@ -26,9 +26,9 @@ public static class LocalizationExtension
 			DisplayLanguage.Hungarian => "Magyar",
 			DisplayLanguage.French => "Français",
 			DisplayLanguage.Chinese => "中文",
-			DisplayLanguage.German => "Deutsch",
-			DisplayLanguage.Portuguese => "Português",
-			DisplayLanguage.Turkish => "Türkçe",
+			// DisplayLanguage.German => "Deutsch",
+			// DisplayLanguage.Portuguese => "Português",
+			// DisplayLanguage.Turkish => "Türkçe",
 			_ => language.ToString()
 		};
 	}
