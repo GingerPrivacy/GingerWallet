cddc418ba25b1c3e395af1c108732016d5118097
Luzius 1089 <luzius1089@protonmail.com>
2026-02-09 22:26:13 +0100

    Update MempoolSpaceExchangeRateProvider.cs

--- GingerCommon/Providers/ExchangeRateProviders/MempoolSpaceExchangeRateProvider.cs
+++ GingerCommon/Providers/ExchangeRateProviders/MempoolSpaceExchangeRateProvider.cs
@@ -17,9 +17,16 @@ public class MempoolSpaceExchangeRateProvider : ExchangeRateProvider
 	public override async Task<Dictionary<string, decimal>> QueryRateAsync(string currency, EndPoint? torEndpoint, CancellationToken cancellationToken)
 	{
 		var contentString = await HttpUtils.HttpGetAsync(ApiUrl, $"api/v1/prices", torEndpoint, null, cancellationToken);
+		var apiUrl = torEndpoint is null
+			? ApiUrl
+			: OnionApiUrl;
+
+		var contentString = await HttpUtils.HttpGetAsync(apiUrl, $"api/v1/prices", torEndpoint, null, cancellationToken);
 		var rates = JsonSerializer.Deserialize<Dictionary<string, decimal>>(contentString, JsonUtils.OptionCaseInsensitive);
 		return rates ?? new();
 	}
 
 	private const string ApiUrl = "https://mempool.space";
+	public const string ApiUrl = "https://mempool.space/";
+	public const string OnionApiUrl = "http://mempoolhqx4isw62xs7abwphsq7ldayuidyx2v2oethdhhj6mlo2r6ad.onion/";
 }
